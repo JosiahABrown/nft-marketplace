2ff9cc45ef649e90646aa17aa150545881ba39de
JosiahABrown <josiah.alen.brown@gmail.com>
2021-10-05 14:19:25 -0700

    Hardhat config and NFT.sol added

---
+++ hardhat.config.js
@@ -0,0 +1,22 @@
+require("@nomiclabs/hardhat-waffle");
+
+const fs = require("fs")
+const privateKey = fs.readFileSync(".secret").toString();
+const projectId = "bccd569a12b440f8bed2a999edcf8a69";
+
+module.exports = {
+  networks: {
+    hardhat: {
+      chainId: 1337
+    },
+    mumbai: {
+      url: 'https://polygon-mumbai.infura.io/v3/${projectId}'
+      accounts: [privateKey]
+    },
+    mainnet: {
+      url: 'https://polygon-mainnet.infura.io/v3/${projectId}'
+      accounts: [privateKey]
+    },
+  }
+  solidity: "0.8.4",
+};
