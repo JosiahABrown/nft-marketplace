05b949e74d63afb743b36c1014472bad09ec3569
JosiahABrown <josiah.alen.brown@gmail.com>
2021-10-08 15:11:45 -0700

    Updated test
    Ran hardhat tests. Updated error in hardhat.config.js .  Cleared default index.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,7 +1,7 @@
 require("@nomiclabs/hardhat-waffle");
 
 const fs = require("fs")
-const privateKey = fs.readFileSync(".secret").toString();
+const privateKey = fs.readFileSync(".secret").toString().trim();
 const projectId = "bccd569a12b440f8bed2a999edcf8a69";
 
 module.exports = {
@@ -10,13 +10,13 @@ module.exports = {
       chainId: 1337
     },
     mumbai: {
-      url: 'https://polygon-mumbai.infura.io/v3/${projectId}'
+      url: 'https://polygon-mumbai.infura.io/v3/${projectId}',
       accounts: [privateKey]
     },
     mainnet: {
-      url: 'https://polygon-mainnet.infura.io/v3/${projectId}'
+      url: 'https://polygon-mainnet.infura.io/v3/${projectId}',
       accounts: [privateKey]
     },
-  }
+  },
   solidity: "0.8.4",
 };
--- test/sample-test.js
+++ test/sample-test.js
@@ -1,19 +1,54 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
-describe("Greeter", function () {
-  it("Should return the new greeting once it's changed", async function () {
-    const Greeter = await ethers.getContractFactory("Greeter");
-    const greeter = await Greeter.deploy("Hello, world!");
-    await greeter.deployed();
+describe("NFTMarket", function () {
+  it("Should create and execute market sales", async function () {
+     //deploy the marketplace
+     const Market = await ethers.getContractFactory("NFTMarket")
+     const market = await Market.deploy()
+     await market.deployed() //waits for the market to be deployed
+     const marketAddress = market.address //once deployed it will grab the address
 
-    expect(await greeter.greet()).to.equal("Hello, world!");
+     //deploy the nft contract
+     const NFT = await ethers.getContractFactory("NFT")
+     const nft = await NFT.deploy(marketAddress)
+     await nft.deployed()
+     const nftContractAddress = nft.address //grabs NFT address once deployed
 
-    const setGreetingTx = await greeter.setGreeting("Hola, mundo!");
+     let listingPrice = await market.getListingPrice()//grabs listing price
+     listingPrice = listingPrice.toString()//turns listing price into a string
 
-    // wait until the transaction is mined
-    await setGreetingTx.wait();
+     const auctionPrice = ethers.utils.parseUnits('100', 'ether') //100 since we're working with matic
 
-    expect(await greeter.greet()).to.equal("Hola, mundo!");
+     //creates two tokens
+     await nft.createToken("https://www.mytokenlocation.com")
+     await nft.createToken("https://www.mytokenlocation2.com")
+
+     //puts both tokens up for sale
+     await market.createMarketItem(nftContractAddress, 1, auctionPrice, { value: listingPrice })
+     await market.createMarketItem(nftContractAddress, 2, auctionPrice, { value: listingPrice })
+
+     /*in a real world example users will be interacting with the contract through something like Metamask.
+     This is a testing environment so we will be using fake local addresses provided by Hardhat*/
+     const [_, buyerAddress] = await ethers.getSigners() 
+
+     //execute sale of token to another user
+     await market.connect(buyerAddress).createMarketSale(nftContractAddress, 1, { value: auctionPrice})
+
+     //query for and return the unsold items
+     let items = await market.fetchMarketItems()
+     
+     items = await Promise.all(items.map(async i => {
+      const tokenUri = await nft.tokenURI(i.tokenId) 
+      let item = {
+        price: i.price.toString(),
+        tokenId: i.tokenId.toString(),
+        seller: i.seller,
+        owner: i.owner,
+        tokenUri
+      }
+      return item
+    }))
+    console.log('items: ', items)
   });
 });
