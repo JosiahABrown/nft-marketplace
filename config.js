2e966938dedfd2d39f02d888ae722646914c829d
JosiahABrown <josiah.alen.brown@gmail.com>
2021-10-26 23:19:36 -0700

    Deployed to Mumbai

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-export const nftaddress = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512"
-export const nftmarketaddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3"
+export const nftaddress = "0x0aeD9C994B1a3056970B5F76C44542BFdE0eD60B"
+export const nftmarketaddress = "0xCD9D3c3bfEe5d4C00D364d6D9A2aE17319fd2Ea4"    
--- hardhat.config.js
+++ hardhat.config.js
@@ -2,7 +2,7 @@ require("@nomiclabs/hardhat-waffle");
 
 const fs = require("fs")
 const privateKey = fs.readFileSync(".secret").toString().trim();
-const projectId = "bccd569a12b440f8bed2a999edcf8a69";
+const projectId = "af695e3666d443c2b8477a2941470fb8";
 
 module.exports = {
   networks: {
@@ -10,11 +10,11 @@ module.exports = {
       chainId: 1337
     },
     mumbai: {
-      url: 'https://polygon-mumbai.infura.io/v3/${projectId}',
+      url: 'https://polygon-mumbai.infura.io/v3/af695e3666d443c2b8477a2941470fb8',
       accounts: [privateKey]
     },
     mainnet: {
-      url: 'https://polygon-mainnet.infura.io/v3/${projectId}',
+      url: 'https://polygon-mainnet.infura.io/v3/af695e3666d443c2b8477a2941470fb8',
       accounts: [privateKey]
     },
   },  
